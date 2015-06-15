85827b2e9f1bd4e4358c00e89863b4753f95b663
Hector Rivas Gandara <keymon@gmail.com>
2015-06-15 17:47:58 +0100

    Add full application deployment smoke test
    Added more tests:
     * Create an app
     * Create a service
     * Push an app

--- spec/endtoend_spec.rb
+++ spec/endtoend_spec.rb
@@ -2,34 +2,71 @@ require 'minigit'
 
 describe "TsuruEndToEnd" do
   context "deploying an application" do
-      before(:all) do
-        @tsuru_home = Tempdir.new('tsuru-command')
-        @tsuru_api_url = "https://ci-api.tsuru.paas.alphagov.co.uk" 
-        @tsuru_api_url_insecure = "http://ci-api.tsuru.paas.alphagov.co.uk:8080" 
-        @tsuru_command = TsuruCommandLine.new(@tsuru_home)  
-        @tsuru_command.target_add("ci", @tsuru_api_url)
-        @tsuru_command.target_add("ci-insecure", @tsuru_api_url_insecure)
-        @tsuru_user = ENV['TSURU_USER']
-        @tsuru_pass = ENV['TSURU_PASS']
-        
-        #@tsuru_example_app_minigit = MiniGit.new(File.join(@tsuru_home, 'tsuru_example_app'))
-        MiniGit.git :clone, "https://github.com/alphagov/flask-sqlalchemy-postgres-heroku-example.git", @tsuru_home.path+"/sampleapp"
-        @tsuru_example_app_minigit = MiniGit.new(File.join(@tsuru_home, 'sampleapp'))
-      end
+    before(:all) do
+      @tsuru_home = Tempdir.new('tsuru-command')
+      @tsuru_api_url = "https://ci-api.tsuru.paas.alphagov.co.uk"
+      @tsuru_api_url_insecure = "http://ci-api.tsuru.paas.alphagov.co.uk:8080"
+      @tsuru_command = TsuruCommandLine.new(@tsuru_home)
+      @tsuru_command.target_add("ci", @tsuru_api_url)
+      @tsuru_command.target_add("ci-insecure", @tsuru_api_url_insecure)
+      @tsuru_user = ENV['TSURU_USER']
+      @tsuru_pass = ENV['TSURU_PASS']
 
-      
-      it "should not be able to login via HTTP" do
-        @tsuru_command.target_set("ci")
-        @tsuru_command.login(@tsuru_user, @tsuru_pass)
-        expect(@tsuru_command.exit_status).to eql 0
-      end
-      it "should be able to login via HTTP" do
-        @tsuru_command.target_set("ci-insecure")
-        @tsuru_command.login(@tsuru_user, @tsuru_pass)
-        expect(@tsuru_command.exit_status).not_to eql 0
-      end
+      # Clone the same app and setup minigit
+      @sampleapp_path = File.join(@tsuru_home, 'sampleapp')
+      MiniGit.git :clone, "https://github.com/alphagov/flask-sqlalchemy-postgres-heroku-example.git", @sampleapp_path
+      @sampleapp_minigit = MiniGit.new(@sampleapp_path)
+            ENV['GIT_PAGER'] = ''
+
+      # Generate the ssh key and setup ssh
+      @ssh_id_rsa_path = File.join(@tsuru_home, '.ssh', 'id_rsa')
+      @ssh_id_rsa_pub_path = File.join(@tsuru_home, '.ssh', 'id_rsa.pub')
+      SshHelper.generate_key(@ssh_id_rsa_path)
+      SshHelper.write_config(File.join(@tsuru_home, '.ssh', 'config'),
+                             { "StrictHostKeyChecking" => "no" } )
     end
- 
+
+    it "should not be able to login via HTTP" do
+      @tsuru_command.target_set("ci-insecure")
+      @tsuru_command.login(@tsuru_user, @tsuru_pass)
+      expect(@tsuru_command.exit_status).not_to eql 0
+    end
+
+    it "should be able to login via HTTPS" do
+      @tsuru_command.target_set("ci")
+      @tsuru_command.login(@tsuru_user, @tsuru_pass)
+      expect(@tsuru_command.exit_status).to eql 0
+    end
+
+    it "should be able to add the ssh key" do
+      @tsuru_command.key_remove('rspec') # Remove previous state if needed
+      @tsuru_command.key_add('rspec', @ssh_id_rsa_pub_path)
+      expect(@tsuru_command.exit_status).to eql 0
+      expect(@tsuru_command.stdout).to match /Key .* successfully added!/
+    end
+
+    it "should be able to create an application" do
+      @tsuru_command.app_remove('sampleapp') # Remove previous state if needed
+      @tsuru_command.app_create('sampleapp', 'python')
+      expect(@tsuru_command.exit_status).to eql 0
+      expect(@tsuru_command.stdout).to match /App .* has been created/
+    end
+
+    it "should be able to create an application" do
+      @tsuru_command.service_remove('sampleapp_db') # Remove previous state if needed
+      @tsuru_command.service_add('postgresql', 'sampleapp_db', 'shared')
+      expect(@tsuru_command.exit_status).to eql 0
+      expect(@tsuru_command.stdout).to match /Service successfully added/
+    end
+
+    it "Should be able to push the application" do
+      git_url = @tsuru_command.get_app_repository('sampleapp')
+      expect(git_url).not_to be_nil
+      @sampleapp_minigit.push(git_url, 'master')
+    end
+
+  end
+
 end
 
 
--- spec/tsuru_helper.rb
+++ spec/tsuru_helper.rb
@@ -1,5 +1,7 @@
 require 'open3'
 require 'tempfile'
+require 'fileutils'
+
 
 class Tempdir < Tempfile
   require 'tmpdir'
@@ -18,51 +20,100 @@ class Tempdir < Tempfile
     rescue Errno::EACCES
       # may not be able to unlink on Windows; just ignore
     end
-  end  
+  end
 end
 
-class TsuruCommandLine 
-    attr_reader :exit_status, :stderr, :stdout
+class SshHelper
+  def self.generate_key(path)
+    FileUtils.mkdir_p(File.dirname(path))
+    system("ssh-keygen -f #{path} -N ''")
+  end
 
-    def initialize(home = ENV['HOME'])   
-      @home = home
-    end
+  def self.write_config(config_path, config)
+    File.open(config_path, 'w') { |f|
+      f.write("Host *\n")
+      config.each { |k,v|
+        f.write("\t#{k} #{v}\n")
+      }
+    }
+  end
+end
 
-    def target_add(target_label, target_url)
-        execute_helper('target-add', target_label, target_url)
-    end
+class TsuruCommandLine
+  attr_reader :exit_status, :stderr, :stdout
 
-    def target_set(target_label)
-        execute_helper('target-set', target_label)
-    end
+  def initialize(home = ENV['HOME'])
+    @home = home
+  end
 
-    def login(login, pass)
-      execute_helper('login', login) do |stdin, out, err, wait_thread|
-        stdin.write(pass + "\n")
-        stdin.flush()
-        stdin.close()
-      end
-    end
-    
-    private
-    
-    def execute_helper(*cmd)
-      puts @home.path
-      @exit_status=nil
-      @stderr=nil
-      @stdout=nil
-      cmd.insert(0,'tsuru')
-      Open3.popen3({ 'HOME' => @home.path }, *cmd) do |stdin, out, err, wait_thread|
-        # Allow additional preprocessing of the system call if the caller passes a block
-        yield(stdin, out, err, wait_thread) if block_given?
+  def target_add(target_label, target_url)
+    execute_helper('target-add', target_label, target_url)
+  end
 
-        @stdout = out.gets("")
-        puts @stdout
-        @stderr = err.gets("")
-        puts @stderr
-        [stdin, out, err].each{|stream| stream.close() if not stream.closed? }
-        @exit_status = wait_thread.value.to_i
-      end
-      return @exit_status == 0
+  def target_set(target_label)
+    execute_helper('target-set', target_label)
+  end
+
+  def app_create(app_name, platform)
+    execute_helper('app-create', app_name, platform)
+  end
+
+  def app_remove(app_name)
+    execute_helper('app-remove', '-a', app_name, '-y')
+  end
+
+  def key_add(ssh_key_name, ssh_key_path)
+    execute_helper('key-add', ssh_key_name, ssh_key_path)
+  end
+
+  def key_remove(ssh_key_name)
+    execute_helper('key-remove', ssh_key_name, '-y')
+  end
+
+  def service_add(service_name, service_instance_name, plan)
+    execute_helper('service-add', service_name, service_instance_name, plan)
+  end
+
+  def service_remove(service_instance_name)
+    execute_helper('service-remove', service_instance_name, '-y')
+  end
+
+  def get_app_repository(app_name)
+    execute_helper('app-info', '-a', app_name)
+    if m = /^Repository: (.*)$/.match(@stdout) then
+      m[1]
+    else
+      nil
     end
+  end
+
+  def login(login, pass)
+    execute_helper('login', login) do |stdin, out, err, wait_thread|
+      stdin.write(pass + "\n")
+      stdin.flush()
+      stdin.close()
+    end
+  end
+
+  private
+
+  def execute_helper(*cmd)
+    puts @home.path
+    @exit_status=nil
+    @stderr=nil
+    @stdout=nil
+    cmd.insert(0,'tsuru')
+    Open3.popen3({ 'HOME' => @home.path }, *cmd) do |stdin, out, err, wait_thread|
+      # Allow additional preprocessing of the system call if the caller passes a block
+      yield(stdin, out, err, wait_thread) if block_given?
+
+      @stdout = out.gets("")
+      puts @stdout
+      @stderr = err.gets("")
+      puts @stderr
+      [stdin, out, err].each{|stream| stream.close() if not stream.closed? }
+      @exit_status = wait_thread.value.to_i
+    end
+    return @exit_status == 0
+  end
 end
