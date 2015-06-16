9b06ef18ac8042e872eb119b88379412315cae3e
Hector Rivas Gandara <keymon@gmail.com>
2015-06-16 17:35:21 +0100

    First implementation of serverspec+Rakefile
    Generate the tasks to run the serverspec tests based on
    a list of hosts (currently hardcoded).
    
    It will run the necessary tests based on the host role.

--- spec/serverspec_helper.rb
+++ spec/serverspec_helper.rb
@@ -2,7 +2,6 @@ require 'serverspec'
 require 'highline/import' # for ask function
 
 set :backend, :ssh
-#set :ssh_options, { :user => ubuntu }
 set :disable_sudo, true
 
 # Load the host
@@ -10,13 +9,16 @@ host = ENV['TARGET_HOST']
 
 # Load the configuration for this host
 # It will scan the default files plus 'ssh.config'
-#options = Net::SSH::Config.for(host, Net::SSH::Config.default_files << './ssh.config')
-options = Net::SSH::Config.for(host, [ './ssh.config' ])
-puts options
-set :ssh_options, options
+options = Net::SSH::Config.for(host, Net::SSH::Config.default_files << './ssh.config')
+
+options[:user] ||= ENV['TARGET_USER']
+options[:port] ||= ENV['TARGET_PORT']
+options[:keys] ||= ENV['TARGET_PRIVATE_KEY']
 
 # Load the host
 set :host,  options[:host_name] || host
+
+set :ssh_options, options
 
 # Set environment variables
 set :env, :LANG => 'C', :LC_MESSAGES => 'C'
