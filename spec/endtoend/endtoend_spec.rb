9267d649f7721a6de0f2e83f4152ed9dcb0f3dcd
Hector Rivas Gandara <keymon@gmail.com>
2015-07-24 15:28:36 +0100

    Add logic to retry to bind service after unbind
    Tsuru executes the unbind operation asynchronously, so if one unbinds
    and binds a service immediately we might hit a race condition. This
    code retries to bind if the service reports being already bound.

--- spec/endtoend/endtoend_spec.rb
+++ spec/endtoend/endtoend_spec.rb
@@ -155,7 +155,14 @@ describe "TsuruEndToEnd" do
       pending "There is already a bug filed: https://github.com/tsuru/postgres-api/issues/1"
       @workspace.tsuru_command.service_unbind(@sampleapp_db_instance, @sampleapp_name)
       expect(@workspace.tsuru_command.exit_status).to eql 0
-      @workspace.tsuru_command.service_bind(@sampleapp_db_instance, @sampleapp_name)
+      retries=5
+      begin
+        sleep 1
+        @workspace.tsuru_command.service_bind(@sampleapp_db_instance, @sampleapp_name)
+        expect(@workspace.tsuru_command.stderr).to_not match /This app is already bound to this service instance/m
+      rescue RSpec::Expectations::ExpectationNotMetError
+        retry if (retries -= 1) > 0
+      end
       expect(@workspace.tsuru_command.exit_status).to eql 0
       expect(@workspace.tsuru_command.stdout).to match /Instance .* is now bound to the app .*/
     end
